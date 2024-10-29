c7d05e5268929528074e251d332cd7bbdd7f8f77
Paradox <123651510+1808goya@users.noreply.github.com>
2024-10-30 00:14:58 +0900

    Test

---
+++ .vs/SW/Project1/Project1/Source.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<time.h>
+#include<conio.h>
+
+int main() {
+
+	printf("Hello World!");
+
+
+	return 0;
+}
